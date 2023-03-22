755b08fdcbbb1be0943e5bf9940a65c95a0a0c96
Danil Murzin <danil.murzin.200026@gmail.com>
2023-03-23 00:50:52 +0300

    add locale db, component setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
+import pizzas from './assets/pizzas.json';
 import './scss/app.scss';
 
 function App() {
@@ -17,14 +18,15 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        <PizzaBlock title="test1"  price={300}/>
-                        <PizzaBlock title="test2"  price="400"/>
-                        <PizzaBlock title="test3"  price="500"/>
+                        {pizzas.map((pizza) => (
+                            <PizzaBlock key={pizza.id} {...pizza} />
+                        ))}
                     </div>
                 </div>
             </div>
         </div>
     );
 }
+// fsaasfasfasf
 
 export default App;
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,26 +1,41 @@
-function PizzaBlock(props) {
+import { useState } from 'react';
+
+function PizzaBlock({ title, price, imageUrl, sizes, types }) {
+    const [activeType, setActiveType] = useState(0);
+    const [activeSize, setActiveSize] = useState(0);
+    const typeNames = ['тонкое', 'традиционное'];
+
     return (
         <div className="pizza-block">
-            <img
-                className="pizza-block__image"
-                src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-                alt="Pizza"
-            />
-            <h4 className="pizza-block__title">{props.title}</h4>
+            <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
+            <h4 className="pizza-block__title">{title}</h4>
             <div className="pizza-block__selector">
                 <ul>
-                    <li className="active">тонкое</li>
-                    <li>традиционное</li>
+                    {types.map((type) => (
+                        <li
+                            key={type}
+                            className={activeType === type ? 'active' : ''}
+                            onClick={() => setActiveType(type)}
+                        >
+                            {typeNames[type]}
+                        </li>
+                    ))}
                 </ul>
                 <ul>
-                    <li className="active">26 см.</li>
-                    <li>30 см.</li>
-                    <li>40 см.</li>
+                    {sizes.map((size, i) => (
+                        <li
+                            key={size}
+                            onClick={() => setActiveSize(i)}
+                            className={activeSize === i ? 'active' : ''}
+                        >
+                            {size} см.
+                        </li>
+                    ))}
                 </ul>
             </div>
             <div className="pizza-block__bottom">
-                <div className="pizza-block__price">от {props.price} ₽</div>
-                <div className="button button--outline button--add">
+                <div className="pizza-block__price">от {price} ₽</div>
+                <button className="button button--outline button--add">
                     <svg
                         width="12"
                         height="12"
@@ -34,8 +49,8 @@ function PizzaBlock(props) {
                         />
                     </svg>
                     <span>Добавить</span>
-                    <i>2</i>
-                </div>
+                    <i>{1}</i>
+                </button>
             </div>
         </div>
     );
