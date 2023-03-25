171ab1f8e71c16929d4c52738ef9716c8b720bec
Danil Murzin <danil.murzin.200026@gmail.com>
2023-03-25 13:20:03 +0300

    add search, add pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,32 +4,46 @@ import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import axios from 'axios';
+import Pagination from '../components/Pagination';
 
-const Home = () => {
+const Home = ({ searchValue }) => {
     const [pizzas, setPizzas] = useState([]);
     const [isLoading, setIsLoadnig] = useState(true);
     const [categoryId, setCategoryId] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
     const [sortType, setSortType] = useState({
         name: 'популярности',
         sortProperty: 'rating',
     });
 
-    const category = categoryId > 0 ? `category=${categoryId}` : '';
-    const sortBy = sortType.sortProperty.replace('-', '');
-    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    // const category = categoryId > 0 ? `category=${categoryId}` : '';
+    // const sortBy = sortType.sortProperty.replace('-', '');
+    // const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    // const search = searchValue ? `&q=${searchValue}` : '';
 
     useEffect(() => {
         setIsLoadnig(true);
         axios
             .get(
-                `http://localhost:3004/items?${category}&_sort=${sortBy}&_order=${order}`
+                `http://localhost:3004/items?_page=${currentPage}&_limit=4${
+                    categoryId > 0 ? `category=${categoryId}` : ''
+                }&_sort=${sortType.sortProperty.replace('-', '')}&_order=${
+                    sortType.sortProperty.includes('-') ? 'asc' : 'desc'
+                }&q=${searchValue ? `${searchValue}` : ''}`
             )
             .then((res) => {
                 setPizzas(res.data);
                 setIsLoadnig(false);
             });
         window.scrollTo(0, 0);
-    }, [categoryId, sortType]);
+    }, [categoryId, sortType, searchValue, currentPage]);
+
+    const skeletons = [...new Array(6)].map((_, index) => (
+        <Skeleton key={index} />
+    ));
+    const items = pizzas.map((pizza) => (
+        <PizzaBlock key={pizza.id} {...pizza} />
+    ));
 
     return (
         <div className="container">
@@ -42,14 +56,9 @@ const Home = () => {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-                {isLoading
-                    ? [...new Array(6)].map((_, index) => (
-                          <Skeleton key={index} />
-                      ))
-                    : pizzas.map((pizza) => (
-                          <PizzaBlock key={pizza.id} {...pizza} />
-                      ))}
+                {isLoading ? skeletons : items}
             </div>
+            <Pagination onPageChange={num => setCurrentPage(num)} />
         </div>
     );
 };
